e658f06faa0f3693ea50257e0bdd28324efb4857
Brandon Evans <bpevans@asu.edu>
2020-11-19 19:06:19 -0500

    First commit
    Basic stuff

---
+++ src/Repository.java
@@ -0,0 +1,20 @@
+import java.util.Observable;
+import java.util.Stack;
+
+public class Repository extends Observable{
+	
+	private double totalDistance;
+	
+	private Repository() {
+		
+	}
+	public Repository getInstance() {
+		
+	}
+	public Stack<Point> getPoints() {
+		
+	}
+	public Stack<Line> getLines() {
+		
+	}
+}
