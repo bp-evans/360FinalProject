b25a92b2eba35bb8a44c5feacf1488901200d27e
Brandon Evans <bpevans@asu.edu>
2020-11-21 18:11:53 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -6,17 +9,20 @@ import javax.swing.JMenuItem;
 
 public class Main extends JFrame {
 	private static JFrame frame;
-	private static commandListener listen;
+	//private static commandListener listen;
 	public Main() {
 		
 	}
 	public static void main(String[] args) {
-		listen = new commandListener();
+		//listen = new commandListener();
 		// Setting up the main JFrame
 		frame = new JFrame("CSE360 Final Project");
         // creating and adding the scroll bar
-        mainDisplay disp = new mainDisplay();
-        frame.add(disp.createScroll());
+		
+        //mainDisplay disp = new mainDisplay(); SCROLL BAR IS DISABLED FOR NOW, NEED TO FIGURE OUT HOW TO ADD STUFF INSIDE IT
+        //frame.add(disp.createScroll());
+		
+		
         frame.setVisible(true);
         frame.setSize(400, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -37,14 +43,28 @@ public class Main extends JFrame {
         file.add(save);
         JMenuItem data = new JMenuItem("Plot Data");
         file.add(data);
+        
+        class commandListener2 implements ActionListener {
+        	public String label;
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		// TODO Auto-generated method stub
+        		if(e.getActionCommand() == "aboutPrint") {
+        			System.out.println("Print the about information");
+        			JLabel label = new JLabel("About stuff");
+        			frame.add(label);
+        		}
+        	}
+        }
 
         //add about tab to menu
+        commandListener2 listen2 = new commandListener2();
         JMenu about = new JMenu("About");
         jMenuBar.add(about);
         JMenuItem aboutPrint = new JMenuItem("About");
         about.add(aboutPrint);
         aboutPrint.setActionCommand("aboutPrint");
-        aboutPrint.addActionListener(listen);
+        aboutPrint.addActionListener(listen2);
       
         
 	}
